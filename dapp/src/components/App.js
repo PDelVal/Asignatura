44a3b8002c1771b6c93ec4d5b7ab25704953b4ac
PDelVal <pabloalpartir@gmail.com>
2020-12-16 12:09:22 +0100

    Algunos cambios mas

--- dapp/src/components/App.js
+++ dapp/src/components/App.js
@@ -1,5 +1,10 @@
 import {DrizzleContext} from "@drizzle/react-plugin";
 
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from "react-router-dom";
 
 import '../css/App.css';
 
@@ -9,6 +14,17 @@ import Alumnos from "./Alumnos";
 import Calificaciones from "./Calificaciones";
 import MisCosas from "./MisCosas";
 
+const Navegacion = () => (
+    <nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/evaluaciones/">Evaluaciones</Link></li>
+            <li><Link to="/alumnos/">Alumnos</Link></li>
+            <li><Link to="/calificaciones/">Calificaciones</Link></li>
+            <li><Link to="/miscosas/">Mis Cosas</Link></li>
+        </ul>
+    </nav>
+);
 
 function App() {
     return (
@@ -20,16 +36,32 @@ function App() {
                     return (<main><h1>⚙️ Cargando dapp...</h1></main>);
                 }
 
-                return (<div className="App"><Header         drizzle={drizzle}drizzleState={drizzleState}/>
-                <Evaluaciones   drizzle={drizzle}drizzleState={drizzleState}/>
-                <Alumnos        drizzle={drizzle}drizzleState={drizzleState}/>
-                <Calificaciones drizzle={drizzle}drizzleState={drizzleState}/>
-                <MisCosas       drizzle={drizzle}drizzleState={drizzleState}/>
-                </div>
-              );
-              }}
-              </DrizzleContext.Consumer>
-            );
-            }
-                
+                return (
+                    <div className="App">
+                        <Router>
+                            <Header drizzle={drizzle} drizzleState={drizzleState}/>
+                            <Navegacion/>
+                            <Route path="/" exact>
+                                <p>Bienvenido a la práctica de BCDA. </p>
+                            </Route>
+                            <Route path="/evaluaciones/">
+                                <Evaluaciones drizzle={drizzle} drizzleState={drizzleState}/>
+                            </Route>
+                            <Route path="/alumnos/">
+                                <Alumnos drizzle={drizzle} drizzleState={drizzleState}/>
+                            </Route>
+                            <Route path="/calificaciones/">
+                                <Calificaciones drizzle={drizzle} drizzleState={drizzleState}/>
+                            </Route>
+                            <Route path="/miscosas/">
+                                <MisCosas drizzle={drizzle} drizzleState={drizzleState}/>
+                            </Route>
+                        </Router>
+                    </div>
+                );
+            }}
+        </DrizzleContext.Consumer>
+    );
+}
+
 export default App;
